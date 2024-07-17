6822ad0d98fefcf25d4680939cfd216a9498e881
malek456 <bouzidimalek01@gmail.com>
2024-07-17 12:54:57 +0100

    add api

--- lib/Chart/Chart.dart
+++ lib/Chart/Chart.dart
@@ -1,12 +1,25 @@
 // app_constants.dart
 
 import 'package:flutter/material.dart';
+import '../data/data.dart';
+
+
+
+Color getBackgroundColor(bool isProd) {
+ if (isProd==true)
+   return Color(0xFFB8E4FF);
+ else
+   return Color(0xFFBDFFB8);
+}
+
+//bool isApiProd = false;
+Color color = getBackgroundColor(isApiProd);
 
 // Define colors
 class AppColors {
   static const Color primaryColor = Color(0xFFFFFFFF);
   static const Color accentColor = Color(0xFF03DAC6);
-  static const Color backgroundColor =  Color(0xFFB8E4FF);
+  static  Color backgroundColor =  color;
   static const Color textColor = Colors.black;
   static const Color buttonColor = Color(0xFF151E93);
   static const Color title =Color(0xFF202244);
--- lib/data/data.dart
+++ lib/data/data.dart
@@ -2,5 +2,5 @@ import 'package:untitled4/models/category_list_model.dart';
 import 'package:untitled4/models/category_model.dart';
 
 
-
-String baseUrl = 'http://192.168.1.95:5000/api';
+bool isApiProd = false;
+String baseUrl = isApiProd ?'http://ec2-107-22-112-49.compute-1.amazonaws.com:5000/'  : 'http://192.168.1.95:5000/api';
